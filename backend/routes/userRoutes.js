68610e0fef54ab76dedfa621e10152c631fad409
james-eo <jameseokonkwo@gmail.com>
2024-09-18 07:04:19 +0100

    Added login and search routes to userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const { registerUser, allUsers } = require("../controllers/userController");
-const { authUser } = require("../controllers/userController");
+const { registerUser, allUsers, authUser } = require("../controllers/userController");
 const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.route("/").post(registerUser);//.get(protect, allUsers);
+router.route("/").post(registerUser).get(protect, allUsers);
 router.post("/login", authUser);
 
 module.exports = router;
