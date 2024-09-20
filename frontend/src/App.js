4e19693437cd629cb4d21c9488fc0dae7fbb500e
james-eo <jameseokonkwo@gmail.com>
2024-09-20 20:37:31 +0100

    Deleted and reinstalled React App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import HomePage from "./Pages/HomePage";
+import ChatPage from "./Pages/ChatPage";
+import { /*BrowserRouter as Router,*/ Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">Home</div>;
+  return (
+    // <Router>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/chats" element={<ChatPage />} />
+      </Routes>
+    </div>
+    // </Router>
+  );
 }
 
 export default App;
