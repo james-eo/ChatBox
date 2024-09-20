d02876c4611b019b54135f269c626b04501c536d
james-eo <jameseokonkwo@gmail.com>
2024-09-20 10:40:10 +0100

    Created basic React App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,7 @@
-import { Route } from "react-router-dom/cjs/react-router-dom.min";
 import "./App.css";
-import HomePage from "./Pages/HomePage";
-import ChatPage from "./Pages/ChatPage";
 
 function App() {
-  return (
-    <div className="App">
-      <Route path="/" component={HomePage} exact />
-      <Route path="/chats" component={ChatPage} />
-    </div>
-  );
+  return <div className="App">Home</div>;
 }
 
 export default App;
-
-// NAMES
-
-// roundtalk
-// roundchat
-// roundmessenger
-// letschat
-//letstalk
