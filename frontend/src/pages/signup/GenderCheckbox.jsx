37ebf47d7ce07051042b0cbf2b9cafdc6f528027
james-eo <jameseokonkwo@gmail.com>
2024-09-25 07:37:35 +0100

    Created pages and their management files

---
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+
+const GenderCheckbox = ({ onCheckboxChange, selectedGender }) => {
+  return (
+    <div className="flex">
+      <div className="form-control">
+        <label
+          className={`label gap-2 cursor-pointer ${
+            selectedGender === "male" ? "selected" : ""
+          }`}
+        >
+          <span className="label-text">Male</span>
+          <input
+            type="checkbox"
+            className="checkbox border-slate-900"
+            checked={selectedGender === "male"}
+            onChange={() => onCheckboxChange("male")}
+          />
+        </label>
+      </div>
+
+      <div className="form-control">
+        <label
+          className={`label gap-2 cursor-pointer ${
+            selectedGender === "female" ? "selected" : ""
+          }`}
+        >
+          <span className="label-text">Female</span>
+          <input
+            type="checkbox"
+            className="checkbox border-slate-900"
+            checked={selectedGender === "female"}
+            onChange={() => onCheckboxChange("female")}
+          />
+        </label>
+      </div>
+
+      <div className="form-control">
+        <label
+          className={`label gap-2 cursor-pointer ${
+            selectedGender === "other" ? "selected" : ""
+          }`}
+        >
+          <span className="label-text">Other</span>
+          <input
+            type="checkbox"
+            className="checkbox border-slate-900"
+            checked={selectedGender === "other"}
+            onChange={() => onCheckboxChange("other")}
+          />
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default GenderCheckbox;
+
+// INITIAL CODE FOR THE GENDER CHECKBOX COMPONENT
+// import React from "react";
+
+// const GenderCheckBox = () => {
+//   return (
+//     <div className="flex">
+//       <div className="form-control">
+//         <label className={`label gap-2 cursor-pointer`}>
+//           <span className="label-text">Male</span>
+//           <input type="checkbox" className="checkbox border-slate-900" />
+//         </label>
+//       </div>
+
+//       <div className="form-control">
+//         <label className={`label gap-2 cursor-pointer`}>
+//           <span className="label-text">Female</span>
+//           <input type="checkbox" className="checkbox border-slate-900" />
+//         </label>
+//       </div>
+
+//       <div className="form-control">
+//         <label className={`label gap-2 cursor-pointer`}>
+//           <span className="label-text">Other</span>
+//           <input type="checkbox" className="checkbox border-slate-900" />
+//         </label>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default GenderCheckBox;
